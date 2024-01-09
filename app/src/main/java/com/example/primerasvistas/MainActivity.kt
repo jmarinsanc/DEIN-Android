fd412d73d341b5269e0ddf96c60ec83eefffce7f
jmarinsanc <jmarinsanc@educacion.navarra.es>
2024-01-09 19:17:26 +0100

    hecha

---
+++ app/src/main/java/com/example/primerasvistas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primerasvistas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
