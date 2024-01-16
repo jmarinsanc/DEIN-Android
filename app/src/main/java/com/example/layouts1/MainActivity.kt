186890cbf1f0fecfba9a94ea5f41432773132140
Juan Pablo Marin <jmarinsanc@educacion.navarra.es>
2024-01-16 15:53:55 +0100

    terminado

--- app/src/main/java/com/example/layouts1/MainActivity.kt
+++ app/src/main/java/com/example/layouts1/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`frame_layout.xml`)
+        setContentView(R.layout.frame_layout)
     }
 }
