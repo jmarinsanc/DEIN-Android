e2c92f54825d8ea21be0a3c4676f260d68821b2e
jmarinsanc <jmarinsanc@educacion.navarra.es>
2024-02-06 17:48:34 +0100

    navbar

---
+++ menu_de_navegacion/app/src/main/java/com/example/menu_de_navegacion/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.menu_de_navegacion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.forEach
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    private val badgeCounts = mutableMapOf<Int, Int>()
+    private val badgeClicks = mutableSetOf<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id. toolbar)
+        setSupportActionBar(toolbar)
+        setupBottomMenu()
+    }
+
+    private fun setupBottomMenu () {
+
+        bottomNavigationView = findViewById(R.id. bottom_navigation)
+        bottomNavigationView .setSelectedItemId(R.id. page_settings)
+
+        bottomNavigationView .setOnItemSelectedListener { item -> onItemSelectedListener(item) }
+
+        bottomNavigationView .menu.forEach { menuItem ->
+            val badge = bottomNavigationView .getOrCreateBadge(menuItem. itemId)
+            badge. isVisible = false
+            badge.badgeGravity = BadgeDrawable. TOP_START
+            badgeCounts[menuItem.itemId] = 0
+        }
+
+
+
+    }
+    private fun onItemSelectedListener (item: MenuItem): Boolean {
+
+        val itemId = item. itemId
+        if (!badgeClicks .contains(itemId)) {
+            val badge = bottomNavigationView .getOrCreateBadge(itemId)
+            badge. isVisible = true
+            badgeClicks .add(itemId)
+        }
+        val currentCount = badgeCounts [itemId] ?: 0
+        badgeCounts [itemId] = currentCount + 1
+        val badge = bottomNavigationView .getOrCreateBadge(itemId)
+        badge.number = badgeCounts [itemId] ?: 0
+
+
+        when (item.itemId) {
+            R.id. page_home -> {
+                showPageFragment(R.drawable. ic_home, R.string. bottom_nav_home)
+                return true
+            }
+            R.id. page_fav -> {
+                showPageFragment(R.drawable. ic_fav, R.string. bottom_nav_fav)
+                return true
+            }
+            R.id. page_search -> {
+                showPageFragment(R.drawable. ic_search, R.string. bottom_nav_search)
+                return true
+            }
+            R.id. page_settings -> {
+                showPageFragment(R.drawable. ic_settings, R.string. bottom_nav_settings)
+                return true
+            }
+            else -> throw IllegalArgumentException( "Item not implemented: ${item.itemId}")
+        }
+    }
+    private fun showPageFragment (@DrawableRes iconId: Int, @StringRes title: Int) {
+        val fragment = PageFragment.newInstance(iconId)
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim. bottom_nav_enter, R.anim. bottom_nav_exit)
+            .replace(R.id. fragmentContainerView, fragment)
+            .commit()
+    }
+}
--- menu_de_navegacion/app/src/main/java/com/example/menu_de_navegacion/pageFragment.kt
+++ menu_de_navegacion/app/src/main/java/com/example/menu_de_navegacion/pageFragment.kt
@@ -1,59 +1,27 @@
-package com.example.menu_de_navegacion
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import com.example.menu_de_navegacion.R
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [pageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class pageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class PageFragment : Fragment() {
+    override fun onCreateView (inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? {
+        val layout = inflater.inflate(R.layout. fragment_page, container, false)
+        val icon = arguments?.getInt(ARG_ICON, 0) ?: 0
+        layout.findViewById<View>(R.id. imageView).setBackgroundResource(icon)
+        return layout
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page2, container, false)
-    }
-
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment pageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            pageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val ARG_ICON = "ARG_ICON"
+        fun newInstance (@DrawableRes iconId: Int): PageFragment {
+            val fragment = PageFragment()
+            val args = Bundle()
+            args.putInt( ARG_ICON, iconId)
+            fragment. arguments = args
+            return fragment
+        }
     }
 }
